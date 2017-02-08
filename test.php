992180d15d537d756442cd347c1b50d7245d83f3
Kevin <lepig@qq.com>
2017-02-08 16:10:12 +0800

    Add Fmt.php

--- test.php
+++ test.php
@@ -3,14 +3,14 @@ include 'util.php';
 date_default_timezone_set('PRC');
 class Test
 {
-	
-	public function run()
-	{
-		$datetime = '2017-2-8 15:57:54';
-		// $datetime = strtotime($datetime);
-		// echo $datetime;
-		echo Util::friendlyDateTime($datetime);
-	}
+    
+    public function run()
+    {
+        $datetime = '2017-2-8 15:57:54';
+        // $datetime = strtotime($datetime);
+        // echo $datetime;
+        echo Util::friendlyDateTime($datetime);
+    }
 }
 
 echo (new Test)->run();
